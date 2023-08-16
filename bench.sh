eeef85067142821fa75e6dcd850d267ad15d92ec
Ivy Zhang <yan3.zhang@intel.com>
2023-08-16 10:14:25 +0800

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -1,4 +1,4 @@
 echo "tuning with 32 cores and batchsize=1"
 export OMP_NUM_THREADS=32
 export KMP_AFFINITY=granularity=fine,noduplicates,compact,1,0
-python benchmark_autoscheduler.py     --network="MLP1"     --batch-size=32     --dtype="float32"     --logdir="tmp_logs_layers"    --profiling=True
+python benchmark_autoscheduler.py     --network="MLP1"     --batch-size=32     --dtype="float32"     --logdir="tmp_logs_layers"
